08183be90e620a583a23ca77e1e4995797822cde
Tsiry Sandratraina <tsiry.sndr@gmail.com>
2023-07-28 17:15:47 +0000

    chore: setup nix flake

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "A Nix-flake-based Bun development environment";
+  
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/master";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem
+    (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in {
+      devShells.default = pkgs.mkShell {
+         buildInputs = [
+          pkgs.bun
+          pkgs.nodejs_18
+         ];
+      };
+    });
+}
