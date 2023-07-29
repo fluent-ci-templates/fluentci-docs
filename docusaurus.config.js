289a50fb7d123a39e81a68f20035bdfad0ddd047
Tsiry Sandratraina <tsiry.sndr@gmail.com>
2023-07-29 04:20:23 +0000

    fix broken links

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -43,7 +43,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            "https://github.com/fluent-ci-templates/fluentci-docs/tree/master/docs",
+            "https://github.com/fluent-ci-templates/fluentci-docs/tree/master",
         },
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
